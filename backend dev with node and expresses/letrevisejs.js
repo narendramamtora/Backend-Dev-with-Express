b8277a6a3560283a5d0c5eca561940e1625462d3
narendra <narendramamtora97@gmail.com>
2023-08-04 22:59:58 +0530

    check

---
+++ backend dev with node and expresses/letrevisejs.js
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+// 1. Write an arrow function which returns the product of two numbers
+let a=(b,v)=> b*v;
+console.log(a(2,3));
+
+
+//  second question = to create the student object
+let student={
+ name: "ram",
+ age: 25,
+details () {
+    console.log('this is '+this.name+' i am '+this.age+' years old')
+ }
+};
+=======
+// 1. Write an arrow function which returns the product of two numbers
+let a=(b,v)=> b*v;
+console.log(a(2,3));
+
+
+//  second question = to create the student object
+let student={
+ name: "ram",
+ age: 25,
+details () {
+    console.log('this is '+this.name+' i am '+this.age+' years old')
+ }
+};
+>>>>>>> c25c8a3777ed3c9b64bae9460e704b4f47f8a2cb
+student.details();
